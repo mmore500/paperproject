219e8b81bd6344d4ed0f7091e0ca914594daee73
Matthew Andres Moreno <mmore500.login@gmail.com>
2024-10-16 16:43:05 +0000

    Add binder submodules

---
+++ script/binder.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Fetch all remote branches
+git fetch --all --jobs $(nproc)
+
+mkdir -p binder
+
+# Loop through remote branches starting with 'binder'
+for branch in $(git branch -r | grep 'origin/binder' | sed 's|origin/||'); do
+    echo "Processing branch $branch"
+
+    # Add the branch as a submodule
+    git submodule add -b "$branch" --depth 1 "$(git config --get remote.origin.url)" "binder/$branch"
+    git config -f .gitmodules submodule."binder/$branch".shallow true
+
+done
+
+# Initialize the submodule
+git submodule update --init --recursive --depth 1 --jobs $(nproc)
