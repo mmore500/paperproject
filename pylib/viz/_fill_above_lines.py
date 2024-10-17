c51dbc08e3fcbd94da9012e385c1f27f8113f09b
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2024-10-16 23:01:13 -0400

    Add size-fixation area plot

---
+++ pylib/viz/_fill_above_lines.py
@@ -0,0 +1,25 @@
+from matplotlib import axes as mpl_axes
+from matplotlib import pyplot as plt
+
+
+# adapted from https://github.com/mwaskom/seaborn/issues/2410#issuecomment-753474050
+def fill_above_lines(
+    ax: mpl_axes.Axes=None,
+    alpha: float=0.2,
+    fill_to: float=1.0,
+    **kwargs,
+) -> None:
+    if ax is None:
+        ax = plt.gca()
+    for line in ax.lines:
+        x, y = line.get_xydata().T
+        ax.fill_between(
+            x,
+            y,
+            fill_to,
+            alpha=alpha,
+            **{
+                "color": line.get_color(),
+                **kwargs,
+            },
+        )
