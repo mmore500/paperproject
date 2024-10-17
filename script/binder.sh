d3e89a666e33dc55d0b37b20fa2999de02f4e5e5
Matthew Andres Moreno <mmore500.login@gmail.com>
2024-10-17 05:04:40 +0000

    Update binder submodules

--- script/binder.sh
+++ script/binder.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+cd "$(dirname "$0")/.."
+
 # Fetch all remote branches
 git fetch --all --jobs $(nproc)
 git fetch origin
@@ -9,6 +11,11 @@ mkdir -p binder
 # Loop through remote branches starting with 'binder'
 for branch in $(git branch -r | grep 'origin/binder' | sed 's|origin/||'); do
     echo "Processing branch $branch"
+
+    if [ -d binder/$branch ]; then
+        echo "Skipping branch $branch"
+        continue
+    fi
 
     # Add the branch as a submodule
     git clone --branch "$branch" --single-branch "https://github.com/mmore500/hypermutator-dynamics.git" "binder/$branch" --jobs $(nproc) --depth 1
