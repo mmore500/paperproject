798967a40f0026947748d6588c920c15af4c4f22
mmore500 <mmore500.login@gmail.com>
2024-10-21 02:22:32 +0000

    Deploying to binder from @ mmore500/paperproject@41395e9302e6d0e36278ce220f688db62fe1eac7 🚀

---
+++ replit.nix
@@ -0,0 +1,14 @@
+{ pkgs }: {
+    deps = [
+      pkgs.python311Packages.pytest
+      pkgs.python310Packages.pytest
+      pkgs.vim
+      pkgs.pandoc
+      pkgs.texlive.combined.scheme-full
+      pkgs.python39Packages.pip
+      pkgs.editorconfig-checker
+      pkgs.python39Packages.editorconfig
+      pkgs.cowsay
+      pkgs.bibtex-tidy
+    ];
+}
