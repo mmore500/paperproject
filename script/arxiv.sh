ee6cb3e0f0d392a2abf5bffcf6abe8d5ae9bb8d1
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2025-08-19 15:09:02 -0400

    Strip dead code from arxiv.sh

--- script/arxiv.sh
+++ script/arxiv.sh
@@ -5,16 +5,8 @@ shopt -s globstar
 
 for binderpath in binder*; do
     echo "binderpath ${binderpath}"
-    find "${binderpath}" -type f ! \( -name "cnorm=log+num-generations=4096+surface-size=256+viz=site-ingest-depth-by-rank-heatmap+ynorm=linear+ext=.png" -o -name "*viz=site-reservation-by-rank-spliced-at-heatmap+ext=.png" -o -name "*.pdf" -o -name "*.tex" -o -name "*.bib" \) -exec rm -f {} +
+    find "${binderpath}" -type f ! \( -name "*.pdf" -o -name "*.tex" -o -name "*.bib" \) -exec rm -f {} +
 done
-
-rm -rf binder/teeplots/0*
-rm -rf binder/teeplots/{10,21,30,40}
-
-# copy symlinks, see https://stackoverflow.com/a/17882368/17332200
-cp -rL implemented_pseudocode implemented_pseudocode_copy
-rm -rf implemented_pseudocode
-mv implemented_pseudocode_copy implemented_pseudocode
 
 find . -type f -name '*.jpg' -exec rm -f {} +
 find . -type d -name dishtiny -exec rm -rf {} +
