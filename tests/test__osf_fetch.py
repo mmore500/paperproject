f304aebe6a321fb16a51daeb73fe481db217fc39
mmore500 <mmore500@users.noreply.github.com>
2026-07-25 15:18:39 +0000

    Deploying to bindle from @ mmore500/paperproject@82328fa24dbe74947e224211ff2605953a61c943 🚀

--- tests/test__osf_fetch.py
+++
@@ -1,7 +0,0 @@
-import pylib
-
-
-def test_osf_fetch():
-    cache_path = pylib.osf_fetch("pvq7w")
-    assert cache_path.exists()
-    assert cache_path.stat().st_size > 0
--- tests/test_pylib.py
+++ tests/test_pylib.py
@@ -1,5 +1,11 @@
-import pylib  # noqa: F401
+import pylib
 
 
 def test_pylib():
     pass
+
+
+def test_osf_fetch():
+    cache_path = pylib.osf_fetch("pvq7w")
+    assert cache_path.exists()
+    assert cache_path.stat().st_size > 0
